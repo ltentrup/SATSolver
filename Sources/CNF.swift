7c15d745041e9ea5cc87614da68b5c2b00f9b325
Leander Tentrup <tentrup@react.uni-saarland.de>
2016-12-09 12:36:15 +0100

    Add initializer to CNF

--- Sources/CNF.swift
+++ Sources/CNF.swift
@@ -3,6 +3,10 @@ public struct CNF<Literal> {
     public typealias Clause = [Literal]
     public var matrix: [Clause]
     
+    init() {
+        matrix = []
+    }
+    
     mutating func add(clause: Clause) {
         matrix.append(clause)
     }
